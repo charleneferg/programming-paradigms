c51ffc85acc4b57c6b8793970928238e8b551202
charlene.ferguson <charlene.ferguson@acquia.com>
2015-03-07 12:26:11 +0000

    add valid maze

--- prolog/printMaze.pl
+++ prolog/printMaze.pl
@@ -1,5 +1,32 @@
-:- consult('maze.pl').
+/*:- consult('maze.pl').*/
 
+mazeSize(5,9).
+barrier(1,8).
+barrier(2,1).
+barrier(2,2).
+barrier(2,4).
+barrier(2,5).
+barrier(3,4).
+barrier(3,7).
+barrier(3,9).
+barrier(4,4).
+barrier(4,7).
+barrier(4,8).
+barrier(4,9).
+barrier(5,2).
+
+
+/* Limit the domain of the maze*/
+
+valid_maze(Row, Col) :- 
+numlist(1, Row, RowRange), 
+numlist(1, Col, ColRange),
+member(Row, RowRange), member(Col, ColRange).
+
+
+/* Below rules produce the output of the maze*/
+
+/* Prints the column numbers */
 print_list([]).
 print_list([H|Y]) :- tab(1), write(H),  print_list(Y).
 
@@ -12,17 +39,22 @@ width(X) :- N is X - 1, write(--), width(N).
 /* How do you create a list of all facts in knowledge base*/
 barrier_list(List) :- findall([X,Y], (barrier(X,Y)), List).
 
-print_barrier :- /*output either . or x depending on member of list */
 
-width2(0).
-width2(X) :- N is X - 1, write(.), tab(1), width2(N).
+/* */
+print_maze(Z) :- barrier_list(List), not(member(Z, List)), write(.).
+print_maze(Z) :- barrier_list(List), (member(Z, List)), write(x).
+/*output either . or x depending on member of list */
+
+width2(0,_).
+/*width2(X) :- N is X - 1, write(.), tab(1), width2(N).*/
+width2(X, Y) :- N is X - 1, print_maze(X, Y), tab(1), width2(N, _).
 
 divider(X) :- write(.+), width(X), write(+).  
 
-row(Y,X,L2) :- numlist(1, Y, L2), print_row(X,L2).
+row(Y,X,L2) :- numlist(1, Y, L2), print_row(Y, X, L2).
 
-print_row(_,[]). 
-print_row(X,[H|T]) :- write(H), write('|'), width2(X), write('|'), nl, print_row(X,T).
+print_row(_, _, []). 
+print_row(Y, X, [H|T]) :- write(H), write('|'), width2(X,Y), write('|'), nl, print_row(Y, X,T).
 
 mazeSize(A,B) :- output(B,A).
 
---
+++ prolog/printMaze2.pl
@@ -0,0 +1,64 @@
+/*:- consult('maze.pl').*/
+mazeSize(5,9).
+barrier(1,8).
+barrier(2,1).
+barrier(2,2).
+barrier(2,4).
+barrier(2,5).
+barrier(3,4).
+barrier(3,7).
+barrier(3,9).
+barrier(4,4).
+barrier(4,7).
+barrier(4,8).
+barrier(4,9).
+barrier(5,2).
+
+add_row(_, [0], _).
+add_row(Row, Col, List) :- N is Col - 1, add_row(Row, [N], [K1]), append(K1,[Row,Col],List).
+
+mazelist(0, _, _).
+mazelist(Row, Col, List) :- add_row(Row, Col, List), 
+							N2 is Row - 1, 
+							mazelist(N2, Col, K2),
+ 							append(K2, [Row,Col], List).
+/* Limit the domain of the maze*/
+
+valid_maze(Row, Col) :- 
+numlist(1, Row, RowRange), 
+numlist(1, Col, ColRange),
+member(Row, RowRange), member(Col, ColRange), mazelist(Row,Col, List).
+
+/* How do you check all facts in knowledge base? */
+/* How do you create a list of all facts in knowledge base*/
+barrier_list(List) :- findall([X,Y], (barrier(X,Y)), List).
+
+/* */
+print_maze(Z) :- barrier_list(List), not(member(Z, List)), write(.).
+print_maze(Z) :- barrier_list(List), (member(Z, List)), write(x).
+/*output either . or x depending on member of list */
+/* Below rules produce the output of the maze             */
+
+/* Prints the column numbers */
+print_list([]).
+print_list([H|Y]) :- tab(1), write(H),  print_list(Y).
+
+column_number(X,L) :- numlist(1, X, L), tab(1), print_list(L).
+
+width(0).
+width(X) :- N is X - 1, write(--), width(N).
+
+width2(0).
+width2(X) :- N is X - 1, write(.), tab(1), width2(N).
+/*width2(X) :- N is X - 1, print_maze(X), tab(1), width2(N). */
+
+divider(X) :- write(.+), width(X), write(+).  
+
+row(Y,X,L2) :- numlist(1, Y, L2), print_row(X, L2).
+
+print_row(_, []). 
+print_row(X, [H|T]) :- write(H), write('|'), width2(X), write('|'), nl, print_row(X,T).
+
+mazeSize(A,B) :- output(B,A).
+
+output(X,Y) :- column_number(X,L), nl, divider(X), nl, row(Y, X, L2), divider(X).
