3eed8970ca20aace4f1acadcf64378cfbc81c1ad
charlene.ferguson <charlene.ferguson@acquia.com>
2015-05-02 12:36:56 +0100

    add next_to

--- prolog/maze.solver.pl
+++ prolog/maze.solver.pl
@@ -1,5 +1,5 @@
 :- consult('maze.pl').
-mazeSize(5,2).
+mazeSize(3,1).
 barrier(1,8).
 barrier(2,1).
 barrier(2,2).
@@ -14,37 +14,34 @@ barrier(4,8).
 barrier(4,9).
 barrier(5,2). 
 
-move(X,Y) :- move([A,B],[C,D]).
-move([A,B],[A,_],[[A,B]]).
-move([A,B],[_,B],[[A,B]]).
-move([A,B],[C,D]):- valid_range(C,D), valid_range(A,B), C > A, Z is C - 1, move([A,B],[Z,D]).
-move([A,B],[C,D]):- valid_range(C,D), valid_range(A,B), A > C, Z is A - 1, move([Z,B],[C,D]).
-move([A,B],[C,D]):- valid_range(C,D), valid_range(A,B), D > B, Z is D - 1, move([A,B],[C,Z]).
-move([A,B],[C,D]):- valid_range(C,D), valid_range(A,B), B > D, Z is B - 1, move([A,Z],[C,D]).
 
-solve(From,To,ActualPath) :- solve1(From,To,[],Path).
+/* Choose the adjacent position*/
+next_to([X,Y], [A,B]) :-  X = A, Y = B + 1.
+next_to([X,Y], [A,B]) :-  X = A, Y = B - 1.
+next_to([X,Y], [A,B]) :-  Y = B, X = A + 1.
+next_to([X,Y], [A,B]) :-  Y = B, Y = A - 1.
+
+
+move([A,B],[C,D]):- valid_range(A,B), valid_range(C,D),  next_to([A,B], [C,D]), C > A, Z is C - 1, move([A,B],[Z,D]).
+move([A,B],[C,D]):- valid_range(A,B), valid_range(C,D),  next_to([A,B], [C,D]), A > C, Z is A - 1, move([Z,B],[C,D]).
+move([A,B],[C,D]):- valid_range(A,B), valid_range(C,D),  next_to([A,B], [C,D]), D > B, Z is D - 1, move([A,B],[C,Z]).
+move([A,B],[C,D]):- valid_range(A,B), valid_range(C,D),  next_to([A,B], [C,D]), B > D, Z is B - 1, move([A,Z],[C,D]).
+
+solve(From,To,Path) :- solve1(From,To,[From],Path).
 solve1(From, From, Route, Route).
-solve1(From,To,Route,Path) :- move(From,To), not(member(From,Route)), solve1(From,To,[From|Route],Path).
-solve1(From,To,Route,Path) :- move(From,Z), not(member(Z,Route)), solve1(Z,To,[Z|Route],Path).
-
-
-collist([0, Col], []).
-collist([Row, Col], [[Row, Col] | L ]) :- 
-							Row > 0, 
-							N2 is Row - 1, 
-							rowlist([N2, Col], L).
-
+/*solve1(From,To,Route,Path) :- move(From,To), not(member(From,Route)), solve1(From,To,[From|Route],Path).*/
+solve1(From,To,Route,Path) :- move(To,Z), not(member(Z,Route)), solve1(From,Z,[Z|Route],Path).
 
 
 rowlist([Row, 0], []).
-rowlist([Row, Col], [[Row, Col] | L ]) :- 
+rowlist([Row, Col], [[Row, Col]| L ]) :- 
 							Col > 0, 
 							N2 is Col - 1, 
 							rowlist([Row, N2], L).
 loop([1, Y], []).
-loop([X, Y],[L |List]) :- X > 0, N3 = Y, N2 is X - 1, rowlist([N2, N3], L), loop([N2, Y], List).						
+loop([X, Y], [L|List]) :- X > 0, N3 = Y, N2 is X - 1, rowlist([N2, N3], L), loop([N2, Y], List).						
 
-mazelist(List) :- mazeSize(X,Y),rowlist([X,Y], Rowlist), loop([X,Y], L1), append(Rowlist,L1,List).
+
 
 
 /* Check that the values are within the range of the maze size*/
@@ -52,23 +49,26 @@ valid_range(X, Y) :-
 mazeSize(Row,Col),
 numlist(1, Row, RowRange), 
 numlist(1, Col, ColRange),
-member(X, RowRange), member(Y, ColRange),not(barrier(X,Y)).
+member(X, RowRange), member(Y, ColRange), not(barrier(X,Y)).
 
-/* How do you create a list of all facts in knowledge base*/
+/* Create a list of all barriers in knowledge base*/
 barrier_list(List) :- 
 	findall([X,Y], 
 	(barrier(X,Y)), List).
+
+/* Maze list of coordinates based on maze size*/
+mazelist(List) :- mazeSize(X,Y), rowlist([X,Y], Rowlist), loop([X,Y], L1), append(Rowlist,L1,List).
 
 /* */
 print_maze(Z) :- 
 	barrier_list(List), 
 	not(member(Z, List)), 
-	write(.).
+	write('.').
 
 print_maze(Z) :- 
 	barrier_list(List), 
 	(member(Z, List)), 
-	write(x).
+	write('x').
 
 
 /*output either . or x depending on member of list */
@@ -81,15 +81,15 @@ print_list([H|Y]) :- tab(1), write(H),  print_list(Y).
 column_number(X,L) :- numlist(1, X, L), tab(1), print_list(L).
 
 width(0).
-width(X) :- N is X - 1, write(--), width(N).
+width(X) :- N is X - 1, write('--'), width(N).
 
 width2(0).
 /*width2(X) :- N is X - 1, write(.), tab(1), width2(N). */
 width2(X) :- N is X - 1, print_maze(X), tab(1), width2(N).
 
-divider(X) :- write(.+), width(X), write(+).  
+divider(X) :- write('.+'), width(X), write('+').  
 
-row(Y,X,L2) :- numlist(1, Y, L2), print_row(X, L2).
+row(Y,X,L2) :- numlist(1, Y, L2), mazelist(Mazelist), print_row(Mazelist, L2).
 
 print_row(_, []). 
 print_row(X, [H|T]) :- write(H), 
