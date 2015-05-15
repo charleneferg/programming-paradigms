9e135bff5b536aef3bcccba7dc828fe5c9fcf98d
charlene.ferguson <charlene.ferguson@acquia.com>
2015-05-15 23:16:24 +0100

    updated

--- racket/sudoku.rkt
+++ racket/sudoku.rkt
@@ -2,7 +2,7 @@
 (require racket/set)
 (require racket/unit)
 (require racket/trace)
-;(require racket/list)
+
 ; define a variable sublist that contains a list
 (define row1 '(0 2 5 0 0 1 0 0 0))
 (define row2 '(1 0 4 2 5 0 0 0 0))
@@ -24,6 +24,16 @@
 '(0 9 1 5 0 0 6 0 0)
 '(0 0 0 0 7 8 1 0 3)
 '(0 0 0 6 0 0 5 9 0)))
+
+(define puzzle3 (list'(0 0 0 3 9 0 7 0 0)
+'(2 0 4 0 7 0 8 0 3)
+'(0 0 3 5 0 0 4 0 6)
+'(0 1 8 0 6 0 5 0 0)
+'(0 2 7 4 0 0 3 8 0)
+'(0 0 9 2 8 0 0 0 0)
+'(3 0 0 0 0 0 0 5 0)
+'(0 0 5 0 1 9 2 0 8)
+'(0 9 2 7 0 5 6 0 0)))
 
 (define small-puzzle (list '(0 2 5 0 0 1 0 0 0)'(1 0 4 2 5 0 0 0 0)))
 
@@ -69,13 +79,28 @@
   (map remove-zero lst))
 
 (define (transform lst)
+  (begin
   (map remove-zero-set lst)
   )
+  )
+
+(define (transform2 lst)
+  (begin
+  (map remove-zero-set lst)
+  )
+  )
+
 
 (solve matrix)
 
+
+
+(define puzzle (transform matrix))
+
 ;; assign matrix with zeros removed and the lists of 9 added
 (define matrix2 (solve matrix))
+
+(transform matrix)
 
 ; result from remove-zero function : removes zero and replaces zero with list possible valus 1-9
 (define r1 '((1 2 3 4 5 6 7 8 9) 2 5 (1 2 3 4 5 6 7 8 9) (1 2 3 4 5 6 7 8 9) 1 (1 2 3 4 5 6 7 8 9) (1 2 3 4 5 6 7 8 9) (1 2 3 4 5 6 7 8 9)))
@@ -102,68 +127,49 @@
 (define set1 (set 1 2 3 4 5 6 7 8 9))
 
 
-(define count-singles
-  (lambda (lst)
-    (cond
-      ((null? lst) 0)
-      ((and (list? (car lst)) (= 1 (length (car lst)))) (+ 1 (count-singles (cdr lst))))
-      (else (+ 0 (count-singles (cdr lst))))
-      )))
 
-;; get row
-(define (getrow matrix x)
-  (list-ref matrix x)
-  )
 
-;; get column
-(define (getcolumn lst)
-  (map first lst))
 
-;(getcolumn matrix)
 
-(transform matrix)
-
-(define puzzle (transform matrix))
 
 
 (define (get-row1 puzzle) 
-  (let ( [row (take puzzle 1)])
+  (let ( [row (flatten (take puzzle 1))])
          row))
 
 (define (get-row2 puzzle) 
-  (let ( [row (drop (take puzzle 2) 1)])
+  (let ( [row (flatten (drop (take puzzle 2) 1))])
          row))
 
 (define (get-row3 puzzle) 
-  (let ( [row (drop (take puzzle 3) 2)])
+  (let ( [row (flatten (drop (take puzzle 3) 2))])
          row))
 
 (define (get-row4 puzzle) 
-  (let ( [row (drop (take puzzle 4) 3)])
+  (let ( [row (flatten (drop (take puzzle 4) 3))])
          row))
 
 (define (get-row5 puzzle) 
-  (let ( [row (drop (take puzzle 5) 4)])
+  (let ( [row (flatten (drop (take puzzle 5) 4))])
          row))
 
 (define (get-row6 puzzle) 
-  (let ( [row (drop (take puzzle 6) 5)])
+  (let ( [row (flatten (drop (take puzzle 6) 5))])
          row))
 
 (define (get-row7 puzzle) 
-  (let ( [row (drop (take puzzle 7) 6)])
+  (let ( [row (flatten (drop (take puzzle 7) 6))])
          row))
 
 (define (get-row8 puzzle) 
-  (let ( [row (drop (take puzzle 8) 7)])
+  (let ( [row (flatten (drop (take puzzle 8) 7))])
          row))
 
 (define (get-row9 puzzle) 
-  (let ( [row (drop (take puzzle 9) 8)])
+  (let ( [row (flatten (drop (take puzzle 9) 8))])
          row))
 
-;row1 col 1(take (car (take puzzle 1)) 1)
-;(take (car (get-row1 puzzle)) 1)
+
 (define (column1 puzzle)
 (let([c1 (take (car (get-row1 puzzle)) 1)]
 [c2 (take (car (get-row2 puzzle)) 1)]
@@ -174,7 +180,7 @@
 [c7 (take (car (get-row7 puzzle)) 1)]
 [c8 (take (car (get-row8 puzzle)) 1)]
 [c9 (take (car (get-row9 puzzle)) 1)])
-  (list c1 c2 c3 c4 c5 c6 c7 c8 c9)
+  (flatten (list c1 c2 c3 c4 c5 c6 c7 c8 c9))
   ))
 
 (define (column2 puzzle)
@@ -187,7 +193,7 @@
 [c7 (drop (take (car (get-row7 puzzle)) 2) 1)]
 [c8 (drop (take (car (get-row8 puzzle)) 2) 1)]
 [c9 (drop (take (car (get-row9 puzzle)) 2) 1)])
-  (list c1 c2 c3 c4 c5 c6 c7 c8 c9)
+  (flatten (list c1 c2 c3 c4 c5 c6 c7 c8 c9))
   ))
 
 (define (column3 puzzle)
@@ -200,7 +206,7 @@
 [c7 (drop (take (car (get-row7 puzzle)) 3) 2)]
 [c8 (drop (take (car (get-row8 puzzle)) 3) 2)]
 [c9 (drop (take (car (get-row9 puzzle)) 3) 2)])
-  (list c1 c2 c3 c4 c5 c6 c7 c8 c9)
+  (flatten (list c1 c2 c3 c4 c5 c6 c7 c8 c9))
   ))
 
 (define (column4 puzzle)
@@ -213,7 +219,7 @@
 [c7 (drop (take (car (get-row7 puzzle)) 4) 3)]
 [c8 (drop (take (car (get-row8 puzzle)) 4) 3)]
 [c9 (drop (take (car (get-row9 puzzle)) 4) 3)])
-  (list c1 c2 c3 c4 c5 c6 c7 c8 c9)
+  (flatten (list c1 c2 c3 c4 c5 c6 c7 c8 c9))
   ))
 
 (define (column5 puzzle)
@@ -226,7 +232,7 @@
 [c7 (drop (take (car (get-row7 puzzle)) 5) 4)]
 [c8 (drop (take (car (get-row8 puzzle)) 5) 4)]
 [c9 (drop (take (car (get-row9 puzzle)) 5) 4)])
-  (list c1 c2 c3 c4 c5 c6 c7 c8 c9)
+  (flatten (list c1 c2 c3 c4 c5 c6 c7 c8 c9))
   ))
 
 (define (column6 puzzle)
@@ -239,7 +245,7 @@
 [c7 (drop (take (car (get-row7 puzzle)) 6) 5)]
 [c8 (drop (take (car (get-row8 puzzle)) 6) 5)]
 [c9 (drop (take (car (get-row9 puzzle)) 6) 5)])
-  (list c1 c2 c3 c4 c5 c6 c7 c8 c9)
+ (flatten (list c1 c2 c3 c4 c5 c6 c7 c8 c9))
   ))
 
 
@@ -253,7 +259,7 @@
 [c7 (drop (take (car (get-row7 puzzle)) 7) 6)]
 [c8 (drop (take (car (get-row8 puzzle)) 7) 6)]
 [c9 (drop (take (car (get-row9 puzzle)) 7) 6)])
-  (list c1 c2 c3 c4 c5 c6 c7 c8 c9)
+  (flatten (list c1 c2 c3 c4 c5 c6 c7 c8 c9))
   ))
 
 (define (column8 puzzle)
@@ -266,7 +272,7 @@
 [c7 (drop (take (car (get-row7 puzzle)) 8) 7)]
 [c8 (drop (take (car (get-row8 puzzle)) 8) 7)]
 [c9 (drop (take (car (get-row9 puzzle)) 8) 7)])
-  (list c1 c2 c3 c4 c5 c6 c7 c8 c9)
+  (flatten (list c1 c2 c3 c4 c5 c6 c7 c8 c9))
   ))
 
 (define (column9 puzzle)
@@ -279,7 +285,7 @@
 [c7 (drop (take (car (get-row7 puzzle)) 9) 8)]
 [c8 (drop (take (car (get-row8 puzzle)) 9) 8)]
 [c9 (drop (take (car (get-row9 puzzle)) 9) 8)])
-  (list c1 c2 c3 c4 c5 c6 c7 c8 c9)
+  (flatten (list c1 c2 c3 c4 c5 c6 c7 c8 c9))
   ))
 
 (define (box1 puzzle)
@@ -287,7 +293,7 @@
 [c2 (take (car (get-row2 puzzle))3)]
 [c3 (take (car (get-row3 puzzle))3)]
 )
-  (list c1 c2 c3)
+  (flatten (list c1 c2 c3))
   ))
 
 (define (box2 puzzle)
@@ -295,7 +301,7 @@
 [c2 (drop (take (car (get-row2 puzzle))6) 3)]
 [c3 (drop (take (car (get-row3 puzzle))6) 3)]
 )
-  (list c1 c2 c3)
+  (flatten (list c1 c2 c3))
   ))
 
 (define (box3 puzzle)
@@ -303,7 +309,7 @@
 [c2 (drop (take (car (get-row2 puzzle))9) 6)]
 [c3 (drop (take (car (get-row3 puzzle))9) 6)]
 )
-  (list c1 c2 c3)
+  (flatten (list c1 c2 c3))
   ))
 
 (define (box4 puzzle)
@@ -312,7 +318,7 @@
 [c5 (take (car (get-row5 puzzle))3)]
 [c6 (take (car (get-row6 puzzle))3)]
 )
-  (list c4 c5 c6)
+  (flatten (list c4 c5 c6))
   ))
 
 (define (box5 puzzle)
@@ -320,7 +326,7 @@
 [c2 (drop (take (car (get-row5 puzzle))6) 3)]
 [c3 (drop (take (car (get-row6 puzzle))6) 3)]
 )
-  (list c1 c2 c3)
+  (flatten (list c1 c2 c3))
   ))
 
 (define (box6 puzzle)
@@ -328,7 +334,7 @@
 [c2 (drop (take (car (get-row5 puzzle))9) 6)]
 [c3 (drop (take (car (get-row6 puzzle))9) 6)]
 )
-  (list c1 c2 c3)
+  (flatten (list c1 c2 c3))
   ))
 
 
@@ -337,7 +343,7 @@
 [c7 (take (car (get-row7 puzzle))3)]
 [c8 (take (car (get-row8 puzzle))3)]
 [c9 (take (car (get-row9 puzzle))3)])
-  (list c7 c8 c9)
+  (flatten (list c7 c8 c9))
   ))
 
 (define (box8 puzzle)
@@ -345,7 +351,7 @@
 [c2 (drop (take (car (get-row8 puzzle))6) 3)]
 [c3 (drop (take (car (get-row9 puzzle))6) 3)]
 )
-  (list c1 c2 c3)
+  (flatten (list c1 c2 c3))
   ))
 
 (define (box9 puzzle)
@@ -353,32 +359,59 @@
 [c2 (drop (take (car (get-row8 puzzle))9) 6)]
 [c3 (drop (take (car (get-row9 puzzle))9) 6)]
 )
-  (list c1 c2 c3)
+  (flatten (list c1 c2 c3))
   ))
 
- ;(flatten (get-row1 puzzle)) result (list (set 1 2 3 4 5 6 7 8 9) (set 2) (set 5) (set 1 2 3 4 5 6 7 8 9) (set 1 2 3 4 5 6 7 8 9) (set 1) (set 1 2 3 4 5 6 7 8 9) (set 1 2 3 4 5 6 7 8 9) (set 1 2 3 4 5 6 7 8 9))
-
+ 
 (define x (flatten (get-row1 puzzle)))
 
-(define single (map (lambda (x) (= 1 (set-count x)))
+(define map-single (map (lambda (x) (= 1 (set-count x)))
                  x))
 
-(define (single? xs) (map (lambda (xs) (= 1 (set-count xs)))
+(define (map-single? xs) (map (lambda (xs) (= 1 (set-count xs)))
                  xs))
 
-(set-subtract (set 1 2 3) (set 2))
-(set-remove (set 1 2) 2)
-(set-first (set 1))
 
+;Gets the single elements from a list
 (define (t xs ys)
   (cond [(null? xs) xs]
         [(= 1 (set-count (car xs))) (cons (car xs) (t (cdr xs) ys))]
         [else (t (cdr xs) ys)]))
-  (trace t)
 
-(define (remove-element xs ys)
-  x
-  )
 
-;(t (flatten (get-row1 puzzle)) '())
-puzzle
+; Stackoverflow get the index of a list
+
+;Gets the position of an element in a list.
+(define (get-index xs y)
+  (let loop (( xs xs)
+             (index 0))
+  (cond[(null? xs) #f]
+       [(equal? (car xs) y) index]
+       (else (loop (cdr xs) (+ 1 index))))))
+
+;(get-index n (set 2)) 
+
+
+;get one single from a list
+(define (z xs ys)
+  (cond [(null? xs) xs]
+        [(= 1 (set-count (car xs))) (car xs) ]
+        [else (t (cdr xs) ys)]))
+
+
+(define values (t (get-row1 puzzle) (set)))
+
+;Store the result of first row in a variable
+(define n (get-row1 puzzle))
+
+
+(define (remove-element xs y)
+  (cond [(null? xs) xs]
+        [(= 1 (set-count (car xs))) (cons (car xs) (remove-element (cdr xs) y))]
+        [(= 1 (set-count y)) (cons (set-subtract (car xs) y) (remove-element (cdr xs) y))]
+        [(set-member? (car xs)  y) (cons (set-subtract (car xs) y) (remove-element (cdr xs) y))]
+        [else (remove-element (cdr xs) y)]))
+ 
+
+;(trace remove-element)
+;(remove-element n (set 2))
